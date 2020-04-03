313f2dfc87e6d8dc5129537577102afc1d58b41b
Alex <chengalex@outlook.com>
2020-04-04 02:17:46 +0800

    Initial Commit

---
+++ spring-boot-zookeeper/src/test/java/com/seth/tutorials/zookeeper/SpringCloudZookeeperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seth.tutorials.zookeeper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudZookeeperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
