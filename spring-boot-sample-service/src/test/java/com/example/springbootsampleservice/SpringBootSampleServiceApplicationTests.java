cb24200634f6dcd97c6eb6b1f5c6fdeffbed7d81
Alex <chengalex@outlook.com>
2020-07-26 09:10:35 +0800

    Fix Test - Could not resolve placeholder 'server.port' in tests

--- spring-boot-sample-service/src/test/java/com/example/springbootsampleservice/SpringBootSampleServiceApplicationTests.java
+++ spring-boot-sample-service/src/test/java/com/example/springbootsampleservice/SpringBootSampleServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.springbootsampleservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = "server.port=8080")
 class SpringBootSampleServiceApplicationTests {
 
 	@Test
